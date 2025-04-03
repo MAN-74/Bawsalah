d60f6e49b339e36119940d65094b9fca5da1ff4a
Danyh9 <444201030@student.ksu.edu.sa>
2025-04-03 06:48:59 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -7,8 +7,8 @@ $host = "localhost";
 $username = "root";   
 $password = "root";       
 $database = "bawsalah"; 
-
-$conn = new mysqli($host, $username, $password, $database);
+$port = 8889;
+$conn = new mysqli($host, $username, $password, $database,$port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
